43215571227de30c61ac704e6f9e99b283e78b0c
HOANG Bao Tin <tinhb92@gmail.com>
2019-08-01 10:46:37 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -50,5 +50,5 @@ def predict():
 if __name__ == '__main__':
     if 'serve' in sys.argv:
         uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
-    app.run()
+    else: app.run()
 
